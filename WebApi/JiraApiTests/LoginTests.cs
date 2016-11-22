e3b3b607d924312d777c0ad87de52edf88aa7f8d
Megan Francis <I59098@xactware.com>
2016-11-22 07:47:15 -0700

    Added a testing project for the solution

---
+++ WebApi/JiraApiTests/LoginTests.cs
@@ -0,0 +1,16 @@
+﻿using JiraApi;
+using NUnit.Framework;
+
+namespace JiraApiTests
+{
+    public class LoginTests
+    {
+        [Test]
+        public void TestBadLogin()
+        {
+            var loginC = new LoginController();
+            var isLoggedIn = loginC.Login("https://epm.verisk.com/jira/", "asdf", "123");
+            Assert.IsFalse(isLoggedIn, "Shouldn't be logged in, but it thinks I am...");
+        }
+    }
+}
---
+++ WebApi/JiraApiTests/TaskTests.cs
@@ -0,0 +1,18 @@
+﻿using JiraApi;
+using NUnit.Framework;
+
+namespace JiraApiTests
+{
+    internal class TaskTests
+    {
+        [Test]
+        public void GetSpecifiedTask()
+        {
+            LoginController lc = new LoginController();
+
+            Assert.IsTrue(lc.Login("https://epm.verisk.com/jira/", "i59098", ""));
+            var tc = new TaskController(lc);
+            var asdf = tc.GetIssue("XWESVC-955");
+        }
+    }
+}
