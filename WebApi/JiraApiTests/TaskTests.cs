026a4f030adda47e93310917f6bd13fdc774dbbd
Megan Francis <I59098@xactware.com>
2016-11-23 13:49:28 -0700

    Added an assert to my task test

--- WebApi/JiraApiTests/TaskTests.cs
+++ WebApi/JiraApiTests/TaskTests.cs
@@ -1,5 +1,6 @@
 ﻿using JiraApi;
 using NUnit.Framework;
+using System.IO;
 
 namespace JiraApiTests
 {
@@ -10,9 +11,10 @@ namespace JiraApiTests
         {
             LoginController lc = new LoginController();
 
-            Assert.IsTrue(lc.Login("https://epm.verisk.com/jira/", "i59098", ""));
+            Assert.IsTrue(lc.Login("https://epm.verisk.com/jira/", "i59098", File.ReadAllText(@"C:\Users\i59098\Google Drive\pword.txt")));
             var tc = new TaskController(lc);
             var asdf = tc.GetIssue("XWESVC-955");
+            Assert.AreEqual(asdf.Key.Value, "XWESVC-955");
         }
     }
 }
