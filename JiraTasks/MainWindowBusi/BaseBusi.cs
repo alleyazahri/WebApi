e8389c40b404c5177f1a3ef2e59af15d71766748
Megan Francis <I59098@xactware.com>
2016-11-23 13:49:04 -0700

    Created a TaskBusi class and moved methods from MainWindow to TaskBUsi

---
+++ JiraTasks/MainWindowBusi/BaseBusi.cs
@@ -0,0 +1,16 @@
+﻿using JiraApi;
+
+namespace JiraTasks.MainWindowBusi
+{
+    internal class BaseBusi
+    {
+        internal TaskController TaskController { get; set; }
+        internal UserController UserController { get; set; }
+
+        public BaseBusi(LoginController loginController)
+        {
+            TaskController = new TaskController(loginController);
+            UserController = new UserController(loginController);
+        }
+    }
+}
---
+++ JiraTasks/MainWindowBusi/TaskBusi.cs
@@ -0,0 +1,49 @@
+﻿using Atlassian.Jira;
+using JiraApi;
+using JiraTasks.Data;
+using System;
+using System.Collections.Generic;
+
+namespace JiraTasks.MainWindowBusi
+{
+    internal class TaskBusi : BaseBusi
+    {
+        public TaskBusi(LoginController loginController) : base(loginController)
+        {
+        }
+
+        internal List<Issue> PopulateJiraTasks()
+        {
+            var filter = new TaskFilter()
+            {
+                Project = new List<string>() { "XWESVC" },
+                UpdatedSince = DateTime.Now.AddMonths(-7),
+                ResolutionDateAfter = DateTime.Now.AddMonths(-3)
+            };
+            return TaskController.GetIssues(filter: filter);
+        }
+
+        internal List<CompoundIssue> CompareTasksToUserPrefs(List<Issue> tasks, Dictionary<string, string> linkedTasks)
+        {
+            var issueList = new List<CompoundIssue>();
+            foreach (var issue in tasks)
+            {
+                if (linkedTasks.ContainsKey(issue.Key.Value))
+                {
+                    issueList.Add(new CompoundIssue() { DevTask = issue, LinkedTask = TaskController.GetIssue(linkedTasks[issue.Key.Value]) });
+                }
+                else
+                {
+                    issueList.Add(new CompoundIssue() { DevTask = issue });
+                }
+            }
+            return issueList;
+        }
+
+        internal void RemoveLink(string maintask, string linkedtask)
+        {
+            Console.WriteLine(maintask);
+            //throw new NotImplementedException();
+        }
+    }
+}
