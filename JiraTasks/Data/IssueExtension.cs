e5c9d73e2a821dd79ff9422161af6a50f6ae1932
Megan Francis <I59098@xactware.com>
2016-11-23 13:45:50 -0700

    Modified IssueExtension by adding a GetIssueSummaryEquivalent method that tells the user if the task is on Beta or Test

--- JiraTasks/Data/IssueExtension.cs
+++ JiraTasks/Data/IssueExtension.cs
@@ -6,7 +6,23 @@ namespace JiraTasks.Data
     {
         public static object[] ToStringArray(this Issue issue)
         {
-            return new object[] { issue.Key.Value, "", issue.Status.Name, issue.Summary, issue.Description };
+            string status = GetIssueSummaryEquivalent(issue.Status.Name);
+            return new object[] { issue.Key.Value, "", status, issue.Summary, issue.Description };
+        }
+
+        private static string GetIssueSummaryEquivalent(string issueSummary)
+        {
+            if (issueSummary == "Functional Testing" || issueSummary == "Integration Testing")
+                return "2 - Test";
+            if (issueSummary == "Acceptance Testing")
+                return "1 - Beta";
+            if (issueSummary == "Code Review")
+                return $"3 - {issueSummary}";
+            if (issueSummary == "In Progress")
+                return $"4 - {issueSummary}";
+            if (issueSummary == "Closed")
+                return $"5 - {issueSummary}";
+            return issueSummary;
         }
     }
 }
