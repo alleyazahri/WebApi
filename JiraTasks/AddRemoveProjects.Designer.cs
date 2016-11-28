19bbe7e6ee41e4a8a31e924dfcf444568b7c42c1
Megan Francis <I59098@xactware.com>
2016-11-28 13:56:41 -0700

    Added a datagridview to view the projects with

--- JiraTasks/AddRemoveProjects.Designer.cs
+++ JiraTasks/AddRemoveProjects.Designer.cs
@@ -28,19 +28,34 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.dgvAddRemoveProjects = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvAddRemoveProjects)).BeginInit();
             this.SuspendLayout();
+            // 
+            // dgvAddRemoveProjects
+            // 
+            this.dgvAddRemoveProjects.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvAddRemoveProjects.Location = new System.Drawing.Point(12, 29);
+            this.dgvAddRemoveProjects.Name = "dgvAddRemoveProjects";
+            this.dgvAddRemoveProjects.RowTemplate.Height = 24;
+            this.dgvAddRemoveProjects.Size = new System.Drawing.Size(656, 220);
+            this.dgvAddRemoveProjects.TabIndex = 0;
             // 
             // AddRemoveProjects
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(680, 261);
+            this.Controls.Add(this.dgvAddRemoveProjects);
             this.Name = "AddRemoveProjects";
             this.Text = "Add/Remove Projects";
+            ((System.ComponentModel.ISupportInitialize)(this.dgvAddRemoveProjects)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.DataGridView dgvAddRemoveProjects;
     }
 }
