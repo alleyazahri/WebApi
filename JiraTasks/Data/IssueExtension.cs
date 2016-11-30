097ce90ae37a3d178566932f3ea6a3da06d2523f
Megan Francis <I59098@xactware.com>
2016-11-30 11:04:09 -0700

    Created a RowColumn class to hold a desired RowColumn value

--- JiraTasks/Data/IssueExtension.cs
+++ JiraTasks/Data/IssueExtension.cs
@@ -6,23 +6,8 @@ namespace JiraTasks.Data
     {
         public static object[] ToStringArray(this Issue issue)
         {
-            string status = GetIssueSummaryEquivalent(issue.Status.Name);
-            return new object[] { issue.Key.Value, "", status, issue.Summary, issue.Description };
-        }
-
-        private static string GetIssueSummaryEquivalent(string issueSummary)
-        {
-            if (issueSummary == "Functional Testing" || issueSummary == "Integration Testing")
-                return "2 - Test";
-            if (issueSummary == "Acceptance Testing")
-                return "1 - Beta";
-            if (issueSummary == "Code Review")
-                return $"3 - {issueSummary}";
-            if (issueSummary == "In Progress")
-                return $"4 - {issueSummary}";
-            if (issueSummary == "Closed")
-                return $"5 - {issueSummary}";
-            return issueSummary;
+            string status = CompoundIssue.GetIssueSummaryEquivalent(issue.Status.Name);
+            return new object[] { issue.Key.Value, "", status, issue.Summary, issue.Description?.Substring(0, issue.Description.Length > 300 ? 300 : issue.Description.Length) };
         }
     }
 }
---
+++ JiraTasks/Data/RowColumn.cs
@@ -0,0 +1,8 @@
+﻿namespace JiraTasks.Data
+{
+    internal class RowColumn
+    {
+        public int Row { get; set; }
+        public int Column { get; set; }
+    }
+}
