3ca95f13f752e0822b5dbaacdb94ac9f3e246eb6
Megan Francis <I59098@xactware.com>
2016-11-30 11:00:50 -0700

    Added buttons for saving and discarding changes when adding and removing projects

--- JiraTasks/AddRemoveProjects.cs
+++ JiraTasks/AddRemoveProjects.cs
@@ -1,20 +1,44 @@
-﻿using System;
+﻿using JiraTasks.Data;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace JiraTasks
 {
     public partial class AddRemoveProjects : Form
     {
-        public AddRemoveProjects()
+        public List<ProjectMenuItem> Projects { get; set; }
+
+        public AddRemoveProjects(List<ProjectMenuItem> projects)
         {
+            Projects = projects;
             InitializeComponent();
+            LoadDataGridView();
+        }
+
+        private void LoadDataGridView()
+        {
+            dgvAddRemoveProjects.Columns.Add("project", "Project");
+            dgvAddRemoveProjects.Columns[0].Width = Width - 100;
+
+            dgvAddRemoveProjects.AllowUserToOrderColumns = true;
+            dgvAddRemoveProjects.Show();
+            foreach (var project in Projects)
+            {
+                dgvAddRemoveProjects.Rows.Add(project.ProjectName);
+            }
+        }
+
+        private void bSaveChanges_Click(object sender, System.EventArgs e)
+        {
+        }
+
+        private void bDiscardChanges_Click(object sender, System.EventArgs e)
+        {
+        }
+
+        private void dgvAddRemoveProjects_CellEndEdit(object sender, DataGridViewCellEventArgs e)
+        {
+            MessageBox.Show(sender.GetType().ToString());
         }
     }
 }
