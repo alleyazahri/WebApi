eed22c13b9aded751459074e182ad5ed17d10a90
Megan Francis <I59098@xactware.com>
2016-12-29 08:56:57 -0700

    Modified the extension method for the jira Issue object

--- JiraTasks/Data/IssueExtension.cs
+++ JiraTasks/Data/IssueExtension.cs
@@ -1,13 +1,22 @@
 ﻿using Atlassian.Jira;
+using System.Collections.Generic;
 
 namespace JiraTasks.Data
 {
     public static class IssueExtension
     {
-        public static object[] ToStringArray(this Issue issue)
+        public static object[] ToObjectArray(this Issue issue, Dictionary<string, string> userNotes)
         {
             string status = CompoundIssue.GetIssueSummaryEquivalent(issue.Status.Name);
-            return new object[] { issue.Key.Value, "", status, issue.Summary, issue.Description?.Substring(0, issue.Description.Length > 300 ? 300 : issue.Description.Length) };
+            return new object[]
+            {
+                issue.Key.Value,
+                "",
+                status,
+                issue.Summary,
+                issue.Description?.Substring(0, issue.Description.Length > 300 ? 300 : issue.Description.Length),
+                userNotes.ContainsKey(issue.Key.Value) ? userNotes[issue.Key.Value] : ""
+            };
         }
     }
 }
