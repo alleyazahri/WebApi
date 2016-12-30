f39642dd8c127bcd20d4c751f7aa1c5bda65d6c0
Megan Francis <I59098@xactware.com>
2016-12-30 12:55:15 -0700

    Added an extension method to and LINQ functionality to parts of the datagridview

---
+++ JiraTasks/MainWindowBusi/DataGridBusi.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace JiraTasks.MainWindowBusi
+{
+    internal static class DataGridBusi
+    {
+        internal static DataGridViewColumn FirstOrDefault(this DataGridViewColumnCollection collection, Func<DataGridViewColumn, bool> lambda)
+        {
+            foreach (var col in collection)
+            {
+                var column = col as DataGridViewColumn;
+                if (lambda(column))
+                {
+                    return column;
+                }
+            }
+            return null;
+        }
+    }
+}
