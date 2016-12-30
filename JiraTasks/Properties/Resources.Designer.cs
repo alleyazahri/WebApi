b38bf43743051c7a5eb3597683bd9e25ea276706
Megan Francis <I59098@xactware.com>
2016-12-30 10:12:38 -0700

    Files I don't understand but that must be important somehow

--- JiraTasks/Properties/Resources.Designer.cs
+++ JiraTasks/Properties/Resources.Designer.cs
@@ -59,5 +59,25 @@ namespace JiraTasks.Properties {
                 resourceCulture = value;
             }
         }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap RefreshButton {
+            get {
+                object obj = ResourceManager.GetObject("RefreshButton", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap RefreshButton1 {
+            get {
+                object obj = ResourceManager.GetObject("RefreshButton1", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
     }
 }
