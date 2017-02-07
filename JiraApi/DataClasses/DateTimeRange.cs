2d3fd8065c3bce7811b12565a6f4a2e6e365e6f5
Megan Francis <I59098@xactware.com>
2017-02-07 14:00:08 -0700

    Added a DateTimeRange class to hold a date and time for filtering

---
+++ JiraApi/DataClasses/DateTimeRange.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace JiraApi.DataClasses
+{
+    public class DateTimeRange
+    {
+        public DateTime? Start { get; set; }
+        public DateTime? End { get; set; }
+
+        public DateTimeRange()
+        {
+            Start = null;
+            End = null;
+        }
+
+        public DateTimeRange(DateTime? start, DateTime? end)
+        {
+            Start = start;
+            End = end;
+        }
+
+        public DateTimeRange DeepCopy()
+        {
+            return new DateTimeRange(Start, End);
+        }
+    }
+}
